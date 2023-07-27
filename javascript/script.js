d4f06d944dc8367260ed3d3a62f854a48aeaaac9
Italo souza sampaio <italo12346@gmail.com>
2023-07-27 15:18:05 -0300

    resolvendo erros do menu

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+var btnMenu = document.querySelector(".btn-menu");
+var menuClick = document.querySelector(".menu");
+btnMenu.addEventListener("click", function(){
+    document.querySelector(".side-bar").classList.toggle("show-menu");
+    document.querySelector(".container").classList.toggle("show-menu");
+});
